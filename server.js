a5472ae9ff60d11570a88c56f79a338a80f9aa8d
Pranjul Sharma <sharma.pranjul1998@gmail.com>
2018-02-16 00:26:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles = {
+   articleOne: {
     title: 'Article One | Pranjul Sharma',
     heading: 'Article One',
     date: 'Feb 16, 2018',
@@ -18,8 +19,24 @@ var articleOne = {
             <p>
                 Hello this is first article page created using hasura imad console by Pranjul Sharma.
             </p>`
+},
+   articleTwo: {
+    title: 'Article Two | Pranjul Sharma',
+    heading: 'Article Two',
+    date: 'Feb 26, 2018',
+    content: `<p>
+                Hello this is second article page created using hasura imad console by Pranjul Sharma.
+            </p>`
+    },
+   articleThree: {
+       title: 'Article Three | Pranjul Sharma',
+    heading: 'Article One',
+    date: 'Feb 16, 2018',
+    content: `<p>
+                Hello this is third article page created using hasura imad console by Pranjul Sharma.
+            </p>`
+   }
 };
-
 function createTemplate(data) {
     var title = data.title;
     var date = data.date;
@@ -59,20 +76,14 @@ function createTemplate(data) {
     
     return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-   res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
