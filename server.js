a46b035e3ac12eed4942a2f4a671bb1017a488bf
Pranjul Sharma <sharma.pranjul1998@gmail.com>
2018-02-15 23:15:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two', function (req, res) {
    res.send("Article two requested and will be served soon."); 
 });
 
-app.get('/article-two', function (req, res) {
+app.get('/article-three', function (req, res) {
    res.send("Article three requested and will be served soon."); 
 });
 
