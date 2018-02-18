7386a48b08d912b26dac49af2b414777e0c143d4
Pranjul Sharma <sharma.pranjul1998@gmail.com>
2018-02-19 00:02:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+//Change the text of the main text
+var element = document.getElementById("main-text");
+
+element.innerHTML = "New Value set using main.js file";
+
+//moving image to left somewhat
+var img = document.getElementById("img");
+
+img.onClick = function() {
+  img.style.marginLeft = "100px";  
+};
