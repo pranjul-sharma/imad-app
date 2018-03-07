b09bc34eec8e3c610f7a6c7930f65fc2200ce636
Pranjul Sharma <sharma.pranjul1998@gmail.com>
2018-03-07 13:39:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,33 @@ app.post('/create-user', function( req, res ){
    });
 });
 
+app.post('/login', function(req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
+      if (err){
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0 ){
+              res.status(403).send("username/password is invalid!");
+          } else {
+              // match password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash (password , salt );
+              
+              if ( hashedPassword ===  dbString ) {
+                  res.send('credentials correct!');
+              } else {
+                  res.status(403).send("username/password invalid!");
+              }
+          }
+      }
+       
+   });    
+});
+
 app.get('/hash/:input', function(req, res) {
    var hashStr = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashStr);
