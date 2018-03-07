390b87d1447310f530f8b63af03fe2f961957478
Pranjul Sharma <sharma.pranjul1998@gmail.com>
2018-03-07 14:03:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ submit.onclick = function(){
     //capture the response and store it in a variable
     request.onreadystatechange = function(){
         
-        if (request.readState === XMLHttpRequest.DONE ) {
+        if (request.readyState === XMLHttpRequest.DONE ) {
             
             //Take some action
             if (request.status === 200 ){
